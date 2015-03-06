cd09c8cf3cf4c34e9e85bd026609323e02cb6493
Mike Ball <mikedball@gmail.com>
2015-03-06 08:23:44 -0500

    add basic SeptaScheduler::Web tests
    TODO: leverage VCR; write more tests

---
+++ spec/lib/septa_scheduler/web_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe SeptaScheduler::Web do
+  describe '/' do
+    before { get '/' }
+
+    it 'is successful' do
+      expect(last_response.status).to eq 200
+    end
+  end
+
+  describe '/point' do
+    before { get '/point?lat=39.946936&lng=-75.235519&route=34' }
+
+    it 'is successful' do
+      expect(last_response.status).to eq 200
+    end
+
+    context 'its response' do
+      before { @response = JSON.parse(last_response.body) }
+
+      context 'the inbound stops' do
+        before { @inbound = @response['inbound'] }
+
+        it 'reports the next 4 scheduled stops' do
+          expect(@inbound.length).to eq 4
+        end
+
+        context 'an inbound stop' do
+          before { @inbound_stop = @inbound[0] }
+
+          it 'reports a StopName' do
+            expect(@inbound_stop['StopName']).to eq 'Baltimore Av & 56th St'
+          end
+        end
+      end
+
+      context 'the outbound stops' do
+        before { @outbound = @response['outbound'] }
+
+        it 'reports the next 4 scheduled stops' do
+          expect(@outbound.length).to eq 4
+        end
+
+        context 'an outbound stop' do
+          before { @outbound_stop = @outbound[0] }
+
+          it 'reports a StopName' do
+            expect(@outbound_stop['StopName']).to eq 'Baltimore Av & 56th St'
+          end
+        end
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 $LOAD_PATH << '../lib'
 
 require 'septa_scheduler'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
 
 RSpec.configure do |config|
+  include Rack::Test::Methods
+
+  def app
+    SeptaScheduler::Web
+  end
 end
